935549e521138f0367f4b82317115a3649d40582
Vishnu Gamini <gamini.vishnu@gmail.com>
2023-04-15 23:33:19 +0530

    error handling

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude,longitude,callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=c6e557f19184d53a39dd0d920c76b268&query='+latitude+','+longitude+'&units=m'
-    request({url,json:true},(error,body = {}) => {
+    request({url,json:true},(error,body) => {
         if(error){
             callback('Unable to connect to weather service!',undefined)
         }
@@ -10,13 +10,19 @@ const forecast = (latitude,longitude,callback) => {
             callback('Unable to find location!',undefined)
         }
         else {
-            callback(undefined,{
-                whole_body:body,
-                temperature:body.current.temperature,
-                chance_of_rain:body.current.precip,
-                weather_description:body.current.weather_descriptions[0],
-                weather_icon: body.current.weather_icons[0]
-            })
+            try{
+                callback(undefined,{
+                temperature:body.body.current.temperature,
+                chance_of_rain:body.body.current.precip,
+                weather_description:body.body.current.weather_descriptions[0],
+                weather_icon: body.body.current.weather_icons[0],
+                whole_body:body
+                })
+
+            } catch(error){
+                callback(error, undefined);
+            }
+            
         }
 
     })
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const geocode = (address,callback) =>{
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ address +'.json?access_token=pk.eyJ1IjoidmlzaG51Z2FtaW5pIiwiYSI6ImNsZXRwMnlkdDFpZjgzcnJ2amNsZmVuOGQifQ.0UBjOavqmPa6Whoz2yYkIg&limit=1'
-    request({url,json:true},(error,{body} = {})=>{
+    request({url,json:true},(error,{body})=>{
         if(error){
             callback('Unable to connect to location services!',undefined)
         }
